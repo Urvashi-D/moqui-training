ebd77bb43889a0b0d81747afd2bec51be9aede43
Urvashi Dhoni <urvashidhoni2@gmail.com>
2025-01-14 11:37:05 +0530

    updated my groovy file

--- script/MoquiTrainingService.groovy
+++ script/MoquiTrainingService.groovy
@@ -1,12 +1,14 @@
 import org.moqui.context.ExecutionContext
 import org.moqui.entity.EntityValue
+
 ExecutionContext ec = context.ec
-// Input parameters
+
+//def trainingId = context.trainingId
 def trainingName = context.trainingName
 def trainingDate = context.trainingDate
 def trainingPrice = context.trainingPrice
 def trainingDuration = context.trainingDuration
-// Validate required fields
+
 if (!trainingName) {
     ec.message.addError("Training name is required.")
     return
@@ -15,22 +17,21 @@ if (!trainingDate) {
     ec.message.addError("Training date is required.")
     return
 }
-// Explicitly generate a unique ID
 def trainingId = ec.entity.sequencedIdPrimary("MoquiTraining", null, null)
 
-// Create the MoquiTraining entity record
 EntityValue trainingRecord = ec.entity.makeValue("MoquiTraining")
 
 trainingRecord.set("trainingId", trainingId) //it is  Explicitly setting trainingId
 trainingRecord.set("trainingName", trainingName)
 trainingRecord.set("trainingDate", trainingDate)
+//trainingRecord.set("trainingPrice", trainingPrice)
+//trainingRecord.set("trainingDuration", trainingDuration)
 
-if (trainingPrice != null) trainingRecord.set("trainingPrice", trainingPrice)
-if (trainingDuration != null) trainingRecord.set("trainingDuration", trainingDuration)
+trainingRecord.set("trainingPrice", trainingPrice != null ? trainingPrice : 0)
+trainingRecord.set("trainingDuration", trainingDuration != null ? trainingDuration: 0)
 
-// Save the record
 trainingRecord = trainingRecord.create()
 
-// Set the output parameter
 context.trainingId = trainingRecord.get("trainingId")
+
 
